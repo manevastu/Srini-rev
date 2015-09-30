63f758366fd0894e93195a1081b5479eb5b533f9
Sush <sushmprabhu@gmail.com>
2015-09-30 16:54:13 +0530

    removed the string compare

--- palin.java
+++ palin.java
@@ -1,27 +1,34 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Stack;
-
+import java.util.*;
 class palin {
-    // srini: test case a*b
 
 	public static void main(String[] args) {
 		// Get the number of testcases
 		Scanner input = new Scanner(System.in);
 		int nt = Integer.parseInt(input.nextLine());
-		ArrayList<Integer> out=new ArrayList<Integer>();
-		String[] rpnArr = new String[nt];
 		int[] result = new int[nt];
 		for(int i=0; i<nt; i++){
 			Integer k = input.nextInt();
 			for (int j=k+1; j<1000000; j++){
-				String pal = String.valueOf(j);
-				StringBuffer valueBuf = new StringBuffer(pal);
-				String value = (valueBuf.reverse()).toString();
-				if(pal.equals(value)) {
+				boolean found = false;
+				Integer number = j; // = and int
+				Stack<Integer> stack = new Stack<Integer>();
+				while (number > 0) {
+				    stack.push( number % 10 );
+				    number = number / 10;
+				}
+				int m=stack.size()-1;
+				for(int l=0;l<stack.size();l++){
+					if(stack.get(l)!=stack.get(m)) break;
+					m--;
+					if(l==(stack.size()-1)){
+						found=true;
+					}
+				}
+				if(found) 
+				{
 					result[i]=j;
 					break;
-				}				
+				}		
 			}
 		}
 		for(int k=0;k<result.length;k++){
